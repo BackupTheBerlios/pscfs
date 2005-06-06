8a1a2cfc886df8dace449f0fabe9be5656f4c2f3
huuhoa <huuhoa>
2005-06-06 13:15:40 +0000

    modifies project settings

--- gccs/run.bat
+++ gccs/run.bat
@@ -1,6 +1,8 @@
 cd bin
 IF "%1"=="ns" (
-	start ns -Djacorb.naming.ior_filename=C:/Work/NS_Ref
+REM ORBInitRef.NameService=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root
+	start ns -Djacorb.naming.ior_filename=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root
+REM	start ns -Djacorb.naming.ior_filename=C:/Work/NS_Ref
 )
 IF "%1"=="server" (
 	start jaco group5.server.GCCSServer
