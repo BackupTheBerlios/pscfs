0789eafc6583a57f7be4642b99081821b83a6d32
huuhoa <huuhoa>
2005-06-08 00:27:31 +0000

    update launch client command

--- gccs/run.bat
+++ gccs/run.bat
@@ -1,13 +1,15 @@
+@echo off
 cd bin
+set CLASSPATH=C:\Work\JacORB\lib\jacorb.jar;..\lib\parlay_interfaces-5.0.jar;..\lib\log4j-1.2.9.jar;.
 IF "%1"=="ns" (
-REM ORBInitRef.NameService=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root
-	start ns -Djacorb.naming.ior_filename=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root
-REM	start ns -Djacorb.naming.ior_filename=C:/Work/NS_Ref
+REM ORBInitRef.NameService=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root %*
+	start ns -Djacorb.naming.ior_filename=corbaloc::localhost:2050/StandardNS/NameServer%2DPOA/_root %*
+REM	start ns -Djacorb.naming.ior_filename=C:/Work/NS_Ref %*
 )
 IF "%1"=="server" (
-	start jaco group5.server.GCCSServer
+	java -classpath %CLASSPATH% group5.server.GCCSServer %*
 )
 IF "%1"=="client" (
-	start jaco group5.client.GCCSClient
+	java -classpath %CLASSPATH% group5.client.GCCSClient %*
 )	
 cd ..
