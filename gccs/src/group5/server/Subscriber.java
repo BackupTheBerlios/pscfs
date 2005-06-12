21e6921ccdcb8a23f6f08c7445fd142752545bae
huuhoa <huuhoa>
2005-06-12 13:45:55 +0000

    Add subscriber management classes

---
+++ gccs/src/group5/server/Subscriber.java
@@ -0,0 +1,96 @@
+/**
+ * 
+ */
+package group5.server;
+
+/**
+ * Represent for a subscriber
+ * 
+ * @author Nguyen Huu Hoa
+ * 
+ */
+public class Subscriber {
+	/**
+	 * the status of the subscriber - reachable - unreachable - busy - ...
+	 */
+	private int status;
+
+	public static final int Reachable = 0;
+
+	public static final int Unreachable = 1;
+
+	public static final int Busy = 2;
+
+	/**
+	 * Address of the subscriber in the network
+	 */
+	private String subscribeAddress;
+
+	/**
+	 * Address of the partner if the subscriber is on the call with other
+	 */
+	private String partnerAddress;
+
+	public Subscriber()
+	{
+		subscribeAddress = "";
+		partnerAddress = "";
+		status = Reachable;
+	}
+	public Subscriber(String subscrAddr)
+	{
+		subscribeAddress = subscrAddr;
+		partnerAddress = "";
+		status = Reachable;
+	}
+	/**
+	 * @param status
+	 *            The status to set.
+	 */
+	public void setStatus(int status) throws org.omg.CORBA.BAD_PARAM {
+		switch (status) {
+		case Reachable:
+		case Unreachable:
+		case Busy:
+			this.status = status;
+			break;
+		default:
+			throw new org.omg.CORBA.BAD_PARAM();
+		}
+	}
+
+	/**
+	 * @return Returns the status.
+	 */
+	public int getStatus() {
+		return status;
+	}
+
+	/**
+	 * @param subscribeAddress The subscribeAddress to set.
+	 */
+	public void setSubscribeAddress(String subscribeAddress) {
+		this.subscribeAddress = subscribeAddress;
+	}
+
+	/**
+	 * @return Returns the subscribeAddress.
+	 */
+	public String getSubscribeAddress() {
+		return subscribeAddress;
+	}
+
+	/**
+	 * @param partnerAddress The partnerAddress to set.
+	 */
+	public void setPartnerAddress(String partnerAddress) {
+		this.partnerAddress = partnerAddress;
+	}
+
+	/**
+	 * @return Returns the partnerAddress.
+	 */
+	public String getPartnerAddress() {
+		return partnerAddress;
+	}
+}
---
+++ gccs/src/group5/server/Subscribers.java
@@ -0,0 +1,63 @@
+/**
+ * 
+ */
+package group5.server;
+
+import java.util.Map;
+
+/**
+ * Collection of subscribers. 
+ * This collection works as a database for collecting all subscribers in the system.
+ * Service can access this databse through functions:
+ * <ol>
+ * <li>Subscribers.getInstance() to get access to instance of the collection
+ * <li>addSubscriber() to add new subscriber to collection
+ * <li>getSubscriber() to get subscriber in collection associated given a subscriber ID
+ * </ol>
+ * @author Nguyen Huu Hoa
+ *
+ */
+public class Subscribers {
+	Map m_listSubscribers;
+	private static int subscriberID = 0;
+	private static Subscribers m_subscribers = new Subscribers();
+
+	public static Subscribers getInstance()
+	{
+		return m_subscribers;
+	}
+	private Subscribers()
+	{
+		
+	}
+	/**
+	 * Add new subscriber, given an address of that subscriber
+	 * @param subscriberAddress Address of the new subscriber
+	 * @return subscriberID associated with newly added subscriber
+	 */
+	public synchronized int addSubscriber(String subscriberAddress)
+	{
+		Subscriber subscr = new Subscriber(subscriberAddress);
+		int subID = getSubscriberID();
+		m_listSubscribers.put(new Integer(subID), subscr);
+		notifyAll();
+		return subID;
+	}
+	/**
+	 * Return the subscriber associated with subscriber ID
+	 * @param subscrID ID of the subscriber
+	 */
+	public synchronized Subscriber getSubscriber(int subscrID)
+	{
+		Subscriber sub = (Subscriber) m_listSubscribers.get(new Integer(subscrID));
+		return sub;
+	}
+	/**
+	 * Get new subscriber ID, unique in the whole application
+	 */
+	private synchronized int getSubscriberID()
+	{
+		subscriberID ++;
+		return subscriberID;
+	}
+}
